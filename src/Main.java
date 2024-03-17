ebcba484247b8cec3763a871590748a1254f8311
Rajath12e <38193362+Rajath12e@users.noreply.github.com>
2024-03-16 21:35:05 -0400

    Java_Practice_Q4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+      /*
+       Write a Java program to display the following pattern.
+Sample Pattern :
+
+   J    a   v     v  a
+   J   a a   v   v  a a
+J  J  aaaaa   V V  aaaaa
+ JJ  a     a   V  a     a
+
+
+  */
+
+
+        System.out.println("      J    a   v     v   a        ");
+        System.out.println("      J   a  a  v   v  a   a       ");
+        System.out.println("  J   J  a a a   v V  a aa  a       ");
+        System.out.println("   JJ   a     a   V  a       a      ");
     }
-}
+
+    }
